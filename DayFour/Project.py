513466809f0b726ccde714dd4a1f7b8f6a6d211c
Chyavan Shenoy <32121448+ChyavanShenoy@users.noreply.github.com>
2022-08-18 05:04:26 +0530

    Day 4 Project Complete

--- DayFour/Project.py
+++ DayFour/Project.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,6 +27,24 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
+# Write your code below this line 👇
+images_ = [rock, paper, scissors]
+user_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors: \n"))
+if user_choice >= 3 or user_choice < 0:
+    print("Invalid choice, you lose!")
+else:
+    print(images_[user_choice])
+    computer_choice = random.randint(0, 2)
+    print(f"Computer chose: \n {images_[computer_choice]}")
 
-user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors: ")
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose!")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif user_choice < computer_choice:
+        print("You lose!")
